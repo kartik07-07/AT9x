dbeaedf3cf7227df4a977f99c99ccd73f24329da
Kartik Bhoyar <kartikbhoyar1234@gmail.com>
2024-10-09 08:03:02 +0530

    MY files

---
+++ src/lab002.java
@@ -0,0 +1,26 @@
+public class lab002 {
+    public static void main(String[]args){
+
+        int a = 7;
+      System.out.printf("%d x 1 = %d", a,a) ;
+      System.out.println();
+      System.out.printf("%d x 2 = %d", a,a * 2) ;
+      System.out.println();
+      System.out.printf("%d x 3 = %d", a,a*3) ;
+      System.out.println();
+      System.out.printf("%d x 4 = %d", a,a*4) ;
+      System.out.println();
+      System.out.printf("%d x 5 = %d", a,a*5) ;
+      System.out.println();
+      System.out.printf("%d x 6 = %d", a,a*6) ;
+      System.out.println();
+      System.out.printf("%d x 7 = %d", a,a*7) ;
+      System.out.println();
+      System.out.printf("%d x 8 = %d", a,a*8) ;
+      System.out.println();
+      System.out.printf("%d x 9 = %d", a,a*9) ;
+      System.out.println();
+      System.out.printf("%d x 10 = %d", a,a*10) ;
+    }
+}
+
