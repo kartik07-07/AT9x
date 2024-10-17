a30328a2f9d574a8dbe655f167bbcb63abe4422c
Kartik Bhoyar <150326782+kartik07-07@users.noreply.github.com>
2024-10-17 13:43:10 +0530

    Update lab0016.java

--- src/lab0016.java
+++ src/lab0016.java
@@ -1,1 +1,11 @@
-
+public class lab0016 {
+    public static void main(String[] args) {
+        Scanner lab0016 = new Scanner(System.in);
+        System.out.println("Enter_num1");
+        int num1= lab0016.nextInt();
+        System.out.println("Enter_num2");
+        int num2= lab0016.nextInt();
+        int maximum_num= (num1>num2 ? num1 : num2);
+        System.out.println("The Maximum num is->"+ maximum_num);
+    }
+}
